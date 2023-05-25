56dd0b8cba6e667f9d6f3bb578096ce89d3c258b
Benthan Vu <bxvu@ucsc.edu>
2023-05-25 15:56:31 -0700

    i dont remember what i added but something changed

--- game.js
+++ game.js
@@ -34,131 +34,137 @@ class Demo extends Phaser.Scene {
                 graphics.strokePoints(points, true);
             }, this);
 
+        console.log(board);
         let g = new Game(this, board);
         g.initializeGame(1);
         this.ui = new UI(this, g);
+        // board.grid.destroy();
+        // graphics.clear()
 
         // this.add.rectangle(500, 500, 100, 100, 0xffffff);
-        board
-        .setInteractive()
-        .on('tiledown', (pointer, tileXY) => {
-            // Phaser.Actions.Call(board.tileZToChessArray(0), (gameObject) => {
-            //     gameObject.destroy();
-            // });
-            // board.forEachTileXY( (tileXY) => {
-            //     // console.log(tileXY.x, tileXY.y);
-            //     // board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
-            //     console.log(board.contains(tileXY.x, tileXY.y, 0));   
-            //   });
-            if (!board.contains(tileXY.x, tileXY.y)) {
-                return;
-            }
+        // board
+        // .setInteractive()
+        // .on('tiledown', (pointer, tileXY) => {
+        //     // Phaser.Actions.Call(board.tileZToChessArray(0), (gameObject) => {
+        //     //     gameObject.destroy();
+        //     // });
+        //     // board.forEachTileXY( (tileXY) => {
+        //     //     // console.log(tileXY.x, tileXY.y);
+        //     //     // board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
+        //     //     console.log(board.contains(tileXY.x, tileXY.y, 0));   
+        //     //   });
+        //     if (!board.contains(tileXY.x, tileXY.y)) {
+        //         return;
+        //     }
 
-            g.players.forEach(player => {
-                if (g.turn[g.currentTurn] == player.id) {
-                    if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
-                        player.move(tileXY.x, tileXY.y);
-                        g.nextTurn();
+        //     g.players.forEach(player => {
+        //         if (g.turn[g.currentTurn] == player.id) {
+        //             if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
+        //                 player.move(tileXY.x, tileXY.y);
+        //                 g.nextTurn();
 
-                        // this.previousLocs.forEach(tile => {
-                            // console.log(board.contains(tile.x, tile.y, 0));   
-                            // board.removeChess(null, tile.x, tile.y, 0,  true);
-                            // board.removeAllChess(true);
+        //                 // this.previousLocs.forEach(tile => {
+        //                     // console.log(board.contains(tile.x, tile.y, 0));   
+        //                     // board.removeChess(null, tile.x, tile.y, 0,  true);
+        //                     // board.removeAllChess(true);
                             
-                        // });
+        //                 // });
     
-                    }
-                    else {
-                        player.move(player.x, player.y);
-                        // player.updateVisual();
-                        // board.removeChess(null, prevLoc.x, prevLoc.y, 0,  true);
-                        // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_LIGHT).setScale(0.7); 
-                    }
-                }
+        //             }
+        //             else {
+        //                 player.move(player.x, player.y);
+        //                 // player.updateVisual();
+        //                 // board.removeChess(null, prevLoc.x, prevLoc.y, 0,  true);
+        //                 // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_LIGHT).setScale(0.7); 
+        //             }
+        //         }
                 
-            });
+        //     });
 
 
-            // board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
-            // g.players.forEach(player => {
-            //     let prevLoc = {x:player.x, y:player.y}
-            //     this.previousLocs.push(prevLoc);
-            //     console.log(this.previousLocs);
-            //     player.move(tileXY.x, tileXY.y);
-            // });
-            // g.players.forEach(player => {
+        //     // board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
+        //     // g.players.forEach(player => {
+        //     //     let prevLoc = {x:player.x, y:player.y}
+        //     //     this.previousLocs.push(prevLoc);
+        //     //     console.log(this.previousLocs);
+        //     //     player.move(tileXY.x, tileXY.y);
+        //     // });
+        //     // g.players.forEach(player => {
                 
-                // g.updateBoard();
-            // });       
-            // board.forEachTileXY( (tileXY) => {
-            //     if (board.contains(tileXY.x, tileXY.y, 0)) {
-            //         console.log(tileXY.x, tileXY.y);
-            //     }
-                // board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
-                // console.log(board.contains(tileXY.x, tileXY.y, 0));   
-            //   });
-            // this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7);
-            // var resultTileXYArray = board.getTileXYAtDirection(tileXY, [0, 1, 2, 3, 4, 5, 6, 7], { end: 1 });
-            // var resultTileXY;
-            // for (var i = 0, cnt = resultTileXYArray.length; i < cnt; i++) {
-            //     resultTileXY = resultTileXYArray[i];
-            //     if (!board.contains(resultTileXY.x, resultTileXY.y)) {
-            //         continue;
-            //     }
-            //     this.rexBoard.add.shape(board, resultTileXY.x, resultTileXY.y, 0, COLOR_LIGHT).setScale(0.7);
-            // }
-        })
-        .on('tileover', (pointer, tileXY) => {
-            // console.log(this.rexBoard);
-            g.players.forEach(player => {
-                // this.previousLocs.forEach(tile => {
-                //     // console.log(board.contains(tile.x, tile.y, 0));   
-                //     board.removeChess(null, tile.x, tile.y, 0,  true);
-                //     // board.removeAllChess(true);
+        //         // g.updateBoard();
+        //     // });       
+        //     // board.forEachTileXY( (tileXY) => {
+        //     //     if (board.contains(tileXY.x, tileXY.y, 0)) {
+        //     //         console.log(tileXY.x, tileXY.y);
+        //     //     }
+        //         // board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
+        //         // console.log(board.contains(tileXY.x, tileXY.y, 0));   
+        //     //   });
+        //     // this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7);
+        //     // var resultTileXYArray = board.getTileXYAtDirection(tileXY, [0, 1, 2, 3, 4, 5, 6, 7], { end: 1 });
+        //     // var resultTileXY;
+        //     // for (var i = 0, cnt = resultTileXYArray.length; i < cnt; i++) {
+        //     //     resultTileXY = resultTileXYArray[i];
+        //     //     if (!board.contains(resultTileXY.x, resultTileXY.y)) {
+        //     //         continue;
+        //     //     }
+        //     //     this.rexBoard.add.shape(board, resultTileXY.x, resultTileXY.y, 0, COLOR_LIGHT).setScale(0.7);
+        //     // }
+        // })
+        // .on('tileover', (pointer, tileXY) => {
+        //     // console.log(this.rexBoard);
+        //     g.players.forEach(player => {
+        //         // this.previousLocs.forEach(tile => {
+        //         //     // console.log(board.contains(tile.x, tile.y, 0));   
+        //         //     board.removeChess(null, tile.x, tile.y, 0,  true);
+        //         //     // board.removeAllChess(true);
                     
-                // });
-                if (g.turn[g.currentTurn] == player.id) {
-                    if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
-                        // board.removeAllChess(true);
-                        board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
-                        // board.removeChess(null, player.x, player.y, 0,  true);
-                        // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_LIGHT).setScale(0.7); 
-                        this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7); 
-                        // console.log(board.contains(tileXY.x, tileXY.y, 0));   
-                    }
-                }
-            });
-        })        
-        .on('tileout', (pointer, tileXY) => {
-            // console.log(this.rexBoard);
-            g.players.forEach(player => {
-                // this.previousLocs.forEach(tile => {
-                //     console.log(board.contains(tile.x, tile.y, 0));   
-                //     board.removeChess(null, tile.x, tile.y, 0,  true);
-                //     // board.removeAllChess(true);
+        //         // });
+        //         if (g.turn[g.currentTurn] == player.id) {
+        //             if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
+        //                 // board.removeAllChess(true);
+        //                 board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
+        //                 // board.removeChess(null, player.x, player.y, 0,  true);
+        //                 // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_LIGHT).setScale(0.7); 
+        //                 this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7); 
+        //                 // console.log(board.contains(tileXY.x, tileXY.y, 0));   
+        //             }
+        //         }
+        //     });
+        // })        
+        // .on('tileout', (pointer, tileXY) => {
+        //     // console.log(this.rexBoard);
+        //     g.players.forEach(player => {
+        //         // this.previousLocs.forEach(tile => {
+        //         //     console.log(board.contains(tile.x, tile.y, 0));   
+        //         //     board.removeChess(null, tile.x, tile.y, 0,  true);
+        //         //     // board.removeAllChess(true);
                     
-                // });
-                if (g.turn[g.currentTurn] == player.id) {
-                    if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
-                        // this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7);    
-                        // board.removeAllChess(true);
-                        board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
-                        // board.removeChess(null, player.x, player.y, 0,  true);
-                        this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_LIGHT).setScale(0.7); 
-                        // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_PRIMARY).setScale(0.7); 
-                        // console.log(board.contains(tileXY.x, tileXY.y, 0));
-                    }
-                }
-            });
-        });
+        //         // });
+        //         if (g.turn[g.currentTurn] == player.id) {
+        //             if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
+        //                 // this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7);    
+        //                 // board.removeAllChess(true);
+        //                 board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
+        //                 // board.removeChess(null, player.x, player.y, 0,  true);
+        //                 this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_LIGHT).setScale(0.7); 
+        //                 // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_PRIMARY).setScale(0.7); 
+        //                 // console.log(board.contains(tileXY.x, tileXY.y, 0));
+        //             }
+        //         }
+        //     });
+        // });
         
         console.log(this);
-
-
+        this.events.on("gainCard", (param) => {
+            this.ui.drawCard(param);
+          });
+        
     }
 
     update() {
         this.ui.updateItemsText();
+        
     }
 }
 
@@ -177,6 +183,13 @@ class UI {
         this.itemsText.setText("Items: " + itemList);
     
     }
+
+    drawCard(card) {
+        let bruh = this.scene.add.rectangle(700, 450, 100, 170, 0xaaff33).setOrigin(0.5);
+        this.scene.add.text(700, 450, card.name, {color:0xFFFFFF}).setOrigin(0.5).setDepth(10);
+        this.scene.add.text(700, 550, card.description, {color:0xFFFFFF}).setOrigin(0.5).setDepth(11);
+    }
+    
 }
 
 class Game {
@@ -222,10 +235,72 @@ class Game {
         //         console.log(event.x, event.y);
         //     });
         // });
+        this.initializeBoard();
 
         this.updateBoard();
         // console.log(this.turn);
         this.startGame();
+    }
+
+    initializeBoard() {
+        
+        this.board
+        .setInteractive()
+        .on('tiledown', (pointer, tileXY) => {
+           
+            if (!this.board.contains(tileXY.x, tileXY.y)) {
+                return;
+            }
+
+            this.players.forEach(player => {
+                if (this.turn[this.currentTurn] == player.id) {
+                    if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
+                        player.move(tileXY.x, tileXY.y);
+                        this.nextTurn();
+                    }
+                    else {
+                        player.move(player.x, player.y);
+                        player.showPossibleSpaces();
+                    }
+                }
+                
+            });
+        })
+        .on('tileover', (pointer, tileXY) => {
+            // console.log(this.rexBoard);
+            this.players.forEach(player => {
+                if (this.turn[this.currentTurn] == player.id) {
+                    if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
+                        // board.removeAllChess(true);
+                        this.board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
+                        // board.removeChess(null, player.x, player.y, 0,  true);
+                        // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_LIGHT).setScale(0.7); 
+                        this.scene.rexBoard.add.shape(this.board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7); 
+                        // console.log(board.contains(tileXY.x, tileXY.y, 0));   
+                    }
+                }
+            });
+        })        
+        .on('tileout', (pointer, tileXY) => {
+            // console.log(this.rexBoard);
+            this.players.forEach(player => {
+
+                if (this.turn[this.currentTurn] == player.id) {
+                    if (player.x >= tileXY.x-1 && player.x <= tileXY.x+1 && player.y <= tileXY.y+1 && player.y >= tileXY.y-1) {
+                        // this.rexBoard.add.shape(board, tileXY.x, tileXY.y, 0, COLOR_PRIMARY).setScale(0.7);    
+                        // board.removeAllChess(true);
+                        this.board.removeChess(null, tileXY.x, tileXY.y, 0,  true);
+                        // board.removeChess(null, player.x, player.y, 0,  true);
+                        this.scene.rexBoard.add.shape(this.board, tileXY.x, tileXY.y, 0, COLOR_LIGHT).setScale(0.7); 
+                        // this.rexBoard.add.shape(board, player.x, player.y, 0, COLOR_PRIMARY).setScale(0.7); 
+                        // console.log(board.contains(tileXY.x, tileXY.y, 0));
+                    }
+                }
+            });
+        });
+        
+        
+        // return board;
     }
 
     startGame() {
@@ -247,6 +322,8 @@ class Game {
         // console.log(this.currentTurn);
         if (this.currentTurn > this.turn.length - 1) {
             this.currentTurn = 0;
+            this.scene.events.emit("gainCard", {name:"test", description:"description"});
+            console.log("Gain card");
         }
         if (this.turn[this.currentTurn] == -1) {
             this.scene.time.delayedCall(500, () => {
@@ -339,8 +416,9 @@ class Game {
     }
 
     playerOverlaps() {
-        if (this.players[0].items.length > 2 && (this.players[0].x == 5 && this.players[0].y == 5) || (this.players[0].x == 5 && this.players[0].y == 6) || (this.players[0].x == 6 && this.players[0].y == 6) || (this.players[0].x == 6 && this.players[0].y == 5)) {
+        if (this.players[0].items.length > 2 && ((this.players[0].x == 5 && this.players[0].y == 5) || (this.players[0].x == 5 && this.players[0].y == 6) || (this.players[0].x == 6 && this.players[0].y == 6) || (this.players[0].x == 6 && this.players[0].y == 5))) {
             this.scene.add.text(500, 500, "You Win!", {color:"#FFFFFF"}).setOrigin(0.5);
+            console.log(this.players[0].items)
             console.log("win");
         }
     }
