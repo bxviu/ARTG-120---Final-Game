b2aa8a6a4b18fe880fb7fc427e7b0ef415024fae
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-05-25 19:12:18 -0700

    theoritcal monster now moves toward player

--- scripts/monster.js
+++ scripts/monster.js
@@ -15,18 +15,58 @@ class Monster extends Entity {
 
     navigate(players) {
         //pass players so that the monster knows where the closest player is.
-        closestPlayer = -1
+        let closestPlayer = -1
+        let closestDistance = 50;
+        //Owen 5/25/2023 find the closest player
         players.array.forEach(element => {
-            
+            let eX = element.x - this.x;
+            let eY = element.y - this.y;
+            let distance = Math.sqrt(Math.abs(eX**2 + eY**2));
+            if (distance < closestDistance) {
+                closestPlayer = element.id;
+                closestDistance = distance;
+            }
         });
 
-        let moveX = Math.random() > 0.5 ? 1 : -1;
+        //Owen 5/25/2023 we know the closest player, figure out which way to move
+        //we will advance on the axis that is farthest from the closest player
+        
+        pX = players[closestPlayer].x;
+        pY = players[closestPlayer].y;
+        
+        dX = pX - this.x;
+        dY = pY - this.y;
+        
+        let moveX = 0;
+        let moveY = 0;
+
+
+        //Owen 5/25/2023 this might be wrong, but might be right
+        if (dX > 0) {
+            moveX++;
+        } 
+        else if (dX < 0) {
+            moveX--;
+        }
+
+        if (dY > 0) {
+            moveX++;
+        } 
+        else if (dY < 0) {
+            moveX--;
+        }
+
+        this.move(this.x + moveX, this.y + moveY);
+
+
+        //Owen 5/25/2023 commented out random move code
+        /*let moveX = Math.random() > 0.5 ? 1 : -1;
         let moveY = Math.random() > 0.5 ? 1 : -1;
         do {
             moveX = Math.random() > 0.5 ? 1 : -1;
             moveY = Math.random() > 0.5 ? 1 : -1;
         } while (this.x+moveX < 0 || this.x+moveX > this.board.width-1 || this.y+moveY < 0 || this.y+moveY > this.board.height-1);
-        this.move(this.x+moveX, this.y+moveY);
+        */
     }
 
     // updateVisual() {    
